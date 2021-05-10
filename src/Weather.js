96bcade5ed35d8fe3bbe2b15bae285d5f29c14db
omarsagoo <om.sagoo@gmail.com>
2021-05-10 08:54:04 -0700

    [add] geolocation support

--- src/Weather.js
+++ src/Weather.js
@@ -3,15 +3,18 @@ import { gql } from '@apollo/client'
 import { client } from './index'
 
 function Weather() {
-    const [ zip, setZip ] = useState('')
+    const [ zip, setZip] = useState('')
     const [ weather, setWeather ] = useState(null)
+    const [unit, setUnit] = useState('')
+    const [lat, setLat] = useState('')
+    const [long, setLong] = useState('')
 
     async function getWeather() {
         try {
           const json = await client.query({
             query: gql`
               query {
-                getWeather(zip:${zip}) {
+                getWeather(zip:${zip}, units:${unit}, lat:${lat}, long:${long}) {
                   temperature
                   description
                   cod
@@ -31,7 +34,7 @@ function Weather() {
 
         {weather 
             ? 
-            weather.data.getWeather.cod != 200 
+            weather.data.getWeather.cod !== 200 
                     ?
                     <h1>{weather.data.getWeather.message}</h1> 
                     :
@@ -44,10 +47,31 @@ function Weather() {
             getWeather()
         }}>
             <input 
-            type={Number}
+            type="number"
             value={zip}
             onChange={(e) => setZip(e.target.value)}
             />
+            <input
+            type="radio"
+            value="metric"
+            onClick={(e) => setUnit(e.target.value)}
+            /> Metric
+            <input
+            type="radio"
+            value="imperial"
+            onClick={(e) => setUnit(e.target.value)}
+            /> Kelvin
+
+            <input
+            type="radio"
+            onClick={(e) => {
+                navigator.geolocation.getCurrentPosition(pos => {
+                    setLat(String(pos.coords[0]))
+                    setLong(String(pos.coords[1]))
+                })
+            }}
+            /> use Coordinates
+            
             <button type="submit">Submit</button>
         </form>
     </div>
